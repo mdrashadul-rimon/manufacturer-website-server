9fc5a7055bc6b6b323e9e73ea7550c6f97e576dd
mdrashadul-rimon <mdrashadul.rimon@gmail.com>
2022-05-23 22:52:52 +0600

    added admin api and created functionalities for admin to update

--- index.js
+++ index.js
@@ -61,6 +61,23 @@ async function run() {
         const users = await userCollection.find().toArray();
         res.send(users);
       });
+
+      app.get('/admin/:email', async (req, res) => {
+        const email = req.params.email;
+        const user = await userCollection.findOne({ email: email });
+        const isAdmin = user.role === 'admin';
+        res.send({ admin: isAdmin })
+      });
+
+      app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
+        const email = req.params.email;
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: 'admin' },
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      })
   
   
       app.get('/booking', verifyJWT, async (req, res) => {
