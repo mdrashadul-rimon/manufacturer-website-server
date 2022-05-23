6a70c703818145e00b377455e36f0be25cd26faf
mdrashadul-rimon <mdrashadul.rimon@gmail.com>
2022-05-23 21:45:05 +0600

    added mongodb uri and created mongodb server

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mut4d.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 app.get('/', (req, res) => {
     res.send('The Server is Ready for Elevator Manufacturer Inc.')
